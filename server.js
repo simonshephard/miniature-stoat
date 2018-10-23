a3731155c3866f610a5adb17d3907160500e1139
Glitch (miniature-stoat) <none>
2018-10-23 23:18:10 +0000

    😭🖖 Checkpoint ./views/index.html:1715671/22 ./server.js:1715671/1979

--- server.js
+++ server.js
@@ -18,6 +18,37 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+
+// **TODO************************
+// on submit I receive file name and size in bytes in JSON
+// use multer npm package to handle file uploading
+
+app.post('/api/fileanalyse', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+
+// OKUser stories:
+// OK-I can submit a form that includes a file upload.
+// OK-The from file input field has the "name" attribute set to "upfile". We rely on this in testing.
+// OK-When I submit something, I will receive the file name and size in bytes within the JSON response
+// OKUsage:
+// OK-Go to the main page, and upload a file using the provided form.
+// OKHint:
+// OK-To handle the file uploading you should use the multer npm package.
+
+// <form enctype="multipart/form-data" method="POST" action="/api/fileanalyse">
+//   <input id="inputfield" type="file" name="upfile">
+//   <input id="button" type="submit" value="Upload">
+// </form>
+
+// Choose and Upload file
+// https://miniature-stoat.glitch.me/api/fileanalyse
+// Cannot POST /api/fileanalyse
+
+
+
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
