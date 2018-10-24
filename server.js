7b626e85cc1558da4259329a897aee7c84abb4b0
Glitch (miniature-stoat) <none>
2018-10-24 11:30:14 +0000

    🦄🍓 Checkpoint ./server.js:1715671/888

--- server.js
+++ server.js
@@ -25,19 +25,41 @@ app.get('/hello', function(req, res){
 // on submit I receive file name and size in bytes in JSON
 // use multer npm package to handle file uploading
 
-app.post('/api/fileanalyse', function(req, res){
-  
+// EXAMPLE
+// 
+// var multer  = require('multer')
+// var upload = multer({ dest: 'uploads/' })
+// 
+// <form action="/profile" method="post" enctype="multipart/form-data">
+//   <input type="file" name="avatar" />
+// </form>
+// 
+// app.post('/profile', upload.single('avatar'), function (req, res, next) {});
+// 
+// req.file is the `avatar` file
+// req.body will hold the text fields, if there were any
+
+
+// START WITH STANDARD
+// app.post('/api/fileanalyse', function(req, res){  
   // first just return greeting
   // res.json({greetings: "Hello, API"});
   // WORKS
-  
   // next try to get req
   // res.json({reqBody: req.body});
   // IS EMPTY
-  
-  
-  
-});
+// });
+
+// NOW ADD upload into post handler
+// app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // same as before - first just return greeting
+  // res.json({greetings: "Hello, API"});
+  // WORKS
+  // and also repeat - next try to get req
+  // res.json({reqBody: req.body});
+  // IS EMPTY {"reqBody": {}}
+
+// });
 
 
 // OKUser stories:
